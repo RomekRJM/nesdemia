eee0c4923a7f526495428f20519eedf00b099a6c
RomekRJM <romek.rjm@gmail.com>
2020-04-25 23:21:17 +0200

    Generate mem map for debugger before compilation.

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo on
+
+D:\ProgramFilesX\Python38\python mem_map.py
+
 C:\cc65\bin\ca65 -g nesdemia.asm
 C:\cc65\bin\ld65 nesdemia.o -o nesdemia.nes -t nes --dbgfile nesdemia.nes.dbg
-D:\ProgramFilesX\Python38\python mem_map.py
+
 C:\fceux\fceux.exe nesdemia.nes
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+python mem_map.py
+
 ca65 nesdemia.asm -o nesdemia.o --debug-info
 ld65 nesdemia.o -o nesdemia.nes -t nes --dbgfile nesdemia.dbgfile
-python mem_map.py
 
 wine ~/fceux/fceux.exe nesdemia.nes
