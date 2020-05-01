e77f3781a6c9a6aeebf693d89a198e98cf72e169
RomekRJM <romek.rjm@gmail.com>
2020-05-01 22:49:29 +0200

    Loop unrolling to reduce VBLANK complexity.

--- nmi.asm
+++ nmi.asm
@@ -1,30 +1,79 @@
 NMI:
   ; reload attribute table
   LDA attributesNeedReloading
-  BEQ CopySpriteDataToPPU
+  BNE CopySpriteDataToPPU
 
   ; Attributes
   LDA $2002             ; read PPU status to reset the high/low latch
-  LDA #$33
+  LDA #$23
   STA $2006             ; write the high byte of $23C0 address
   LDA #$C0
   STA $2006             ; write the low byte of $23C0 address
   LDA #$00
   STA attributesNeedReloading
 
-  LDX #$00
-
 CopyAttributeDataToPPU:
-  LDA BEGIN_OF_ATTRIBUTES_MEMORY, X
-  STA $2007          ; write to PPU
-  INX
-  CPX #$41
-  BNE CopyAttributeDataToPPU
+  LDA #$ff
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  LDA $d2
+  STA $2007
+  LDA $d3
+  STA $2007
+  LDA $d4
+  STA $2007
+  LDA $d5
+  LDA #$ff
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  LDA $da
+  STA $2007
+  LDA $db
+  STA $2007
+  LDA $dc
+  STA $2007
+  LDA $dd
+  STA $2007
+  LDA #$ff
+  STA $2007
+  STA $2007
+  STA $2007
+  STA $2007
+  LDA $e2
+  STA $2007
+  LDA $e3
+  STA $2007
+  LDA $e4
+  STA $2007
+  LDA $e5
+  STA $2007
 
 CopySpriteDataToPPU:
   ; copy sprite data from $0200 => PPU memory for display
   LDA #$02
   STA $4014
+
   INC frame
   INC nmiTimer
+
   RTI
