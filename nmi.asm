f33cdb039c8be438379405e2a4063704c9b0a419
Roman Subik <roman.subik@ocado.com>
2020-07-19 18:53:04 +0200

    Proof that new algorithm should work.

--- nmi.asm
+++ nmi.asm
@@ -16,6 +16,20 @@ NMI:
   DEC countdownTimer
   LDA countdownTimer
   BNE :+
+    LDA $2002             ; read PPU status to reset the high/low latch
+    LDA #$20
+    STA $2006             ; write the high byte of $2000 address
+    LDA #$e0
+    STA $2006             ; write the low byte of $2000 address
+
+    LDA frame
+    AND #%00001111
+    STA $2007
+    BIT $2002
+    LDA #$00
+    STA $2005
+    STA $2005
+
     LDA #GAME_TIME_UNIT
     STA countdownTimer
     INC refreshBackground
