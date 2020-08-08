ebfb058410cecf11f4f2154ed63018e5908fa32e
RomekRJM <romek.rjm@gmail.com>
2020-08-08 12:07:04 +0200

    #10 Use precompiled GGSound.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 D:\ProgramFilesX\Python38\python mem_map.py
 
 C:\cc65\bin\ca65 -g nesdemia.asm
-C:\cc65\bin\ld65 nesdemia.o -o nesdemia.nes -t nes --dbgfile nesdemia.nes.dbg
+C:\cc65\bin\ld65 nesdemia.o -o nesdemia.nes music/ggsound.o -C nes.cfg --dbgfile nesdemia.nes.dbg
 
 C:\fceux\fceux.exe nesdemia.nes
