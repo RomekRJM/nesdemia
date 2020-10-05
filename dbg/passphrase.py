2d666dea6e53785c71e3162abc5c6e1421b47f3c
RomekRJM <romek.rjm@gmail.com>
2020-10-05 15:35:25 +0200

    #7 Add money to passphrase.

--- dbg/passphrase.py
+++ dbg/passphrase.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 import itertools
 import unittest
 
-def encode(speed, luck, attack, level):
-    bin_passphrase = "{}{}{}{}".format(int_to_bin(speed, 3), int_to_bin(luck, 3),
-    int_to_bin(attack, 3), int_to_bin(level, 5))
-    control_sum = int_to_bin(sum([speed, luck, attack, level]), 6)
+
+def encode(money, speed, luck, attack, level):
+    bin_passphrase = "{}{}{}{}{}".format(int_to_bin(money, 8), int_to_bin(speed, 3), int_to_bin(luck, 3),
+                                         int_to_bin(attack, 3), int_to_bin(level, 5))
+    control_sum = int_to_bin(compute_control_sum(money, speed, luck, attack, level), 6)
 
     return hex(int(bin_passphrase + control_sum, 2))
 
@@ -13,47 +15,58 @@ def int_to_bin(value, width):
     return bin(value)[2:].zfill(width)
 
 
+def compute_control_sum(money, speed, luck, attack, level):
+    control_sum = sum([money, speed, luck, attack, level])
+    return control_sum if control_sum <= 255 else control_sum - 256
+
+
 def decode(passphrase):
-  bin_passphrase = format(int(passphrase, 16), '0>20b')
-  speed = int(bin_passphrase[0:3], 2)
-  luck = int(bin_passphrase[3:6], 2)
-  attack = int(bin_passphrase[6:9], 2)
-  level = int(bin_passphrase[9:14], 2)
-  control_sum = int(bin_passphrase[14:], 2)
+    bin_passphrase = format(int(passphrase, 16), '0>28b')
+    money = int(bin_passphrase[0:8], 2)
+    speed = int(bin_passphrase[8:11], 2)
+    luck = int(bin_passphrase[11:14], 2)
+    attack = int(bin_passphrase[14:17], 2)
+    level = int(bin_passphrase[17:22], 2)
+    control_sum = int(bin_passphrase[22:], 2)
 
-  if sum([speed, luck, attack, level]) != control_sum:
-      raise AttributeError("Invalid passphrase")
+    if compute_control_sum(money, speed, luck, attack, level) != control_sum:
+        raise AttributeError("Invalid passphrase")
 
-  return (speed, luck, attack, level)
+    return (money, speed, luck, attack, level)
 
 
 class TestGeneration(unittest.TestCase):
-  def test_all_possibilities(self):
-    speed = list(range(0,8))
-    luck = list(range(0,8))
-    attack = list(range(0,8))
-    level = list(range(0,32))
+    def test_all_possibilities(self):
+        money = list(range(0, 256))
+        speed = list(range(0, 8))
+        luck = list(range(0, 8))
+        attack = list(range(0, 8))
+        level = list(range(0, 32))
 
-    for sp, lu, at, lv in itertools.product(*[speed, luck, attack, level]):
-      encoded = encode(sp, lu, at, lv)
-      print("{}, speed={}, luck={}, attack={}, level={}".format(encoded[2:].upper(), sp, lu, at, lv))
+        for sp, lu, at, mo, lv in itertools.product(*[money, speed, luck, attack, level]):
+            encoded = encode(mo, sp, lu, at, lv)
+            # print("{}, money={}, speed={}, luck={}, attack={}, level={}".format(encoded[2:].upper(), mo, sp, lu, at, lv))
 
-      self.assertListEqual([sp, lu, at, lv], list(decode(encoded)))
+            self.assertListEqual([mo, sp, lu, at, lv], list(decode(encoded)))
 
-  def test_decode_wrong_passphrase(self):
-    self.assertRaises(AttributeError, decode, '0x1ab9c')
+    def test_decode_wrong_passphrase(self):
+        self.assertRaises(AttributeError, decode, '0x1ab9c88')
 
-  def test_most_pastphrases_are_wrong(self):
-    all = 2 ** 20
-    wrong = 0
+    def test_most_pastphrases_are_wrong(self):
+        all = 2 ** 28
+        wrong = 0
+        percent = 0
 
-    for i in range(all):
-      try:
-        decode(hex(i))
-      except AttributeError:
-        wrong += 1
+        for i in range(all):
+            try:
+                decode(hex(i))
+                if i % 2684354 == 0:
+                    print('{}: Checked {} percent.'.format(datetime.utc_now(), percent))
+                    percent += 1
+            except AttributeError:
+                wrong += 1
 
-    self.assertTrue(float(wrong) / all > 0.75)
+        self.assertTrue(float(wrong) / all > 0.75)
 
 
 if __name__ == '__main__':
