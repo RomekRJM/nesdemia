5a28242d0a9afbaacf74f51025506a64148fdb26
RomekRJM <romek.rjm@gmail.com>
2020-10-10 23:35:33 +0200

    #11 Tools for nametable generation.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python mem_map.py
+python dbg/mem_map.py
 
 ca65 nesdemia.asm -o nesdemia.o --debug-info
 ld65 nesdemia.o -o nesdemia.nes music/ggsound.o -C nes.cfg --dbgfile nesdemia.dbgfile
